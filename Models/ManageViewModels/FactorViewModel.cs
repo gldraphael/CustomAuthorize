fdeaa89bc891b5e902e97070fe374f23eb43c288
Galdin Raphael <gldraphael@gmail.com>
2016-12-29 12:02:41 +0530

    Initial Commit

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAuthorize.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
